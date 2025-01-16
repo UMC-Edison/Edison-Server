3f173c904aa06ee0d35ac53af37ce6917f9e04a9
chaeone02 <sandy12150831@gmail.com>
2025-01-16 18:42:14 +0900

    bug: 라벨 삭제시 연관된 bubblelabel도 삭제되도록 수정

--- project/src/main/java/com/edison/project/domain/label/entity/Label.java
+++ project/src/main/java/com/edison/project/domain/label/entity/Label.java
@@ -1,9 +1,12 @@
 package com.edison.project.domain.label.entity;
 
+import com.edison.project.domain.bubble.entity.BubbleLabel;
 import com.edison.project.domain.member.entity.Member;
 import com.edison.project.global.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.*;
 
 @Getter
 @Setter
@@ -27,5 +30,10 @@ public class Label extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "member_id") // 로그인 안한 유저 id값 없다면, nullable=true 추가
     private Member member;
+
+
+    // BubbleLabel과 연관관계 설정 -> Label 삭제 시 연관된 BubbleLabel도 삭제
+    @OneToMany(mappedBy = "label", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private Set<BubbleLabel> bubbleLabels = new HashSet<>();
 
 }
