42460f6d46c28fb53e177baf88df126f0dccf0ec
OneE <bestoneeee@naver.com>
2025-01-17 15:07:19 +0900

    Update BubbleService.java

--- project/src/main/java/com/edison/project/domain/bubble/service/BubbleService.java
+++ project/src/main/java/com/edison/project/domain/bubble/service/BubbleService.java
@@ -1,11 +1,19 @@
 package com.edison.project.domain.bubble.service;
 
+import com.edison.project.common.response.ApiResponse;
 import com.edison.project.domain.bubble.dto.BubbleRequestDto;
 import com.edison.project.domain.bubble.dto.BubbleResponseDto;
 import com.edison.project.global.security.CustomUserPrincipal;
+import com.edison.project.domain.bubble.entity.Bubble;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.data.domain.Pageable;
+import java.util.List;
 
 public interface BubbleService {
     BubbleResponseDto.CreateResultDto createBubble(CustomUserPrincipal userPrincipal, BubbleRequestDto.CreateDto requestDto);
     BubbleResponseDto.DeleteResultDto deleteBubble(CustomUserPrincipal userPrincipal, Long bubbleId);
     BubbleResponseDto.RestoreResultDto restoreBubble(CustomUserPrincipal userPrincipal, Long bubbleId);
+
+    ResponseEntity<ApiResponse> getBubblesByMember(Long memberId, Pageable pageable);
 }
