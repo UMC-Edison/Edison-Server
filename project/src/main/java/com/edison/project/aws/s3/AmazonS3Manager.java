5a3196f98a29eb5d4cce01d0d346eb8a31ebd6e8
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-02-19 20:06:34 +0900

    feature: S3 initial settings

---
+++ project/src/main/java/com/edison/project/aws/s3/AmazonS3Manager.java
@@ -0,0 +1,21 @@
+package com.edison.project.aws.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.edison.project.global.config.AmazonConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AmazonS3Manager{
+
+    private final AmazonS3 amazonS3;
+
+    private final AmazonConfig amazonConfig;
+
+    public String uploadFile(String keyName, MultipartFile file){ return null;}
+
+}
---
+++ project/src/main/java/com/edison/project/global/config/AmazonConfig.java
@@ -0,0 +1,42 @@
+package com.edison.project.global.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AWSCredentials awsCredentials() {
+        return new BasicAWSCredentials(accessKey, secretKey);
+    }
+
+    @Bean
+    public AWSCredentialsProvider awsCredentialsProvider(AWSCredentials awsCredentials) {
+        return new AWSStaticCredentialsProvider(awsCredentials);
+    }
+
+    @Bean
+    public AmazonS3 amazonS3(AWSCredentialsProvider awsCredentialsProvider) {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(awsCredentialsProvider)
+                .build();
+    }
+}
