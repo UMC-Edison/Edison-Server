0e96c34685c022fa90c21ddfc1745fe829b17fc8
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-04-01 17:14:40 +0900

    chore: import 오류 수정

--- project/src/main/java/com/edison/project/global/common/s3/AmazonS3Manager.java
+++ project/src/main/java/com/edison/project/global/common/s3/AmazonS3Manager.java
@@ -2,9 +2,9 @@ package com.edison.project.global.common.s3;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.edison.project.global.config.AmazonConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,6 +17,9 @@ import java.util.UUID;
 public class AmazonS3Manager {
 
     private final AmazonS3 amazonS3;
+
+    @Autowired
+    private AmazonS3Manager amazonS3Manager;
     private final AmazonConfig amazonConfig;
 
     public String uploadFile(String type, Long username, MultipartFile file) {
