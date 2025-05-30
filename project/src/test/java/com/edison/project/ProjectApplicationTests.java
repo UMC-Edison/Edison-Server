8532aea676ec515b1773042a3704dd1756047992
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-05-30 11:32:47 +0900

    fix: Update ProjectApplicationTests.java

--- project/src/test/java/com/edison/project/ProjectApplicationTests.java
+++ project/src/test/java/com/edison/project/ProjectApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 
-@SpringBootTest
+// @SpringBootTest
 @ImportAutoConfiguration(
   exclude = org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration.class
 )
