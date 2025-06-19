6d6b0552f0713666aab6ce786f4aefe2eb5fd6f3
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-06-19 21:09:10 +0900

    feat: presigned get url 설계

--- project/src/main/java/com/edison/project/global/common/s3/S3Controller.java
+++ project/src/main/java/com/edison/project/global/common/s3/S3Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/s3")
+@RequestMapping("/s3")
 public class S3Controller {
 
     private final S3Service s3Service;
