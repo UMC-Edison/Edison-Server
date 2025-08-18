9538e214e94a5376456c87459a1d40a32774c1e9
OneE <bestoneeee@naver.com>
2025-08-18 23:45:52 +0900

    fix: ObjectMapper에 JavaTimeModule 추가하여 LocalDateTime 직렬화 문제 해결

--- project/src/main/java/com/edison/project/global/config/AppConfig.java
+++ project/src/main/java/com/edison/project/global/config/AppConfig.java
@@ -1,6 +1,9 @@
 package com.edison.project.global.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +19,9 @@ public class AppConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return new ObjectMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
